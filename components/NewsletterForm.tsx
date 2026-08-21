22b12b1f3929f97bbc58f29d9b9ca06c01433933
ecb12345 <cwynarus@gmail.com>
2026-08-21 16:46:49 +0200

    Naprawiono widoczność pól i przycisków formularza MailerLite

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -4,13 +4,24 @@ import { useEffect } from "react";
 
 export function NewsletterForm({ formId = "E23vqZ" }: { formId?: string }) {
   useEffect(() => {
-    if (typeof window !== "undefined" && (window as any).ml) {
-      try {
-        (window as any).ml('account', '2343740');
-      } catch (err) {
-        console.error(err);
+    const initMailerLite = () => {
+      if (typeof window !== "undefined" && (window as any).ml) {
+        try {
+          (window as any).ml('account', '2343740');
+        } catch (err) {
+          console.error(err);
+        }
       }
-    }
+    };
+
+    initMailerLite();
+    const timer1 = setTimeout(initMailerLite, 300);
+    const timer2 = setTimeout(initMailerLite, 1000);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
   }, [formId]);
 
   return (
